2d40c66bf9a0b11561545b6ae057334d8001733e
David Ramos <dirherrera@hotmail.com>
2020-10-16 15:42:14 -0700

    cambios

--- NetCoreApp/Controllers/CustomerRecordController.cs
+++ NetCoreApp/Controllers/CustomerRecordController.cs
@@ -1,16 +1,33 @@
-﻿using System;
+﻿using NetCoreApp.Models;
+using NetCoreApp.Security;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
+using System.Data;
+using System.Text;
+using System.Text.Json;
 
 namespace NetCoreApp.Controllers
 {
-	public class CustomerRecordController : Controller
+	public class CustomerRecordController : Microsoft.AspNetCore.Mvc.Controller
 	{
-		public IActionResult Index()
+
+		public Microsoft.AspNetCore.Mvc.IActionResult Index()
 		{
+			if (!Credential.IsLoggedIn(HttpContext))
+				return RedirectToAction("Index", "Home", null);
+
+
+
 			return View();
 		}
+		
+		public string SearchCustomer(string term)
+		{
+			string query = $"Select Top 10 * From [Customer] Where Name LIKE '{term}%'";
+			DataTable table = DataAccess.Fill(query);
+			List<Customer> customers = DataTableAdapter.Get<Customer>(table);
+
+			return JsonSerializer.Serialize(customers);
+		}
+
 	}
 }
